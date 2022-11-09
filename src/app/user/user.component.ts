ea756fefcf33d28c3454d39b3a927c72ef9a21b0
Saban Ünlü [netTrek] <us@netTrek.de>
2022-11-09 11:12:44 +0100

    component basics

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,12 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ViewEncapsulation} from '@angular/core';
 
 @Component({
   selector: 'proleit-user',
   templateUrl: './user.component.html',
-  styleUrls: ['./user.component.scss']
+  // template: '<h2>{{name}}</h2>',
+  styleUrls: ['./user.component.scss'],
+  /*styles: [
+    `
+      h2 {
+        color: aqua;
+      }
+    `
+  ]*/
+  encapsulation: ViewEncapsulation.Emulated,
 })
 export class UserComponent {
+  name = 'saban';
+  age?: number;
 
   constructor() { }
 
+  chgName() {
+    this.name = 'peter';
+  }
 }
