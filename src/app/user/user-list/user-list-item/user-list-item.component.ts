47a7e31a53b56958e044cc830d042c8301f9651e
Saban Ünlü [netTrek] <us@netTrek.de>
2022-11-10 09:51:51 +0100

    userlist part 2 HostBinding

--- src/app/user/user-list/user-list-item/user-list-item.component.ts
+++ src/app/user/user-list/user-list-item/user-list-item.component.ts
@@ -1,15 +1,20 @@
-import {Component, EventEmitter, HostListener, Input, OnInit, Output} from '@angular/core';
+import {Component, EventEmitter, HostBinding, HostListener, Input, OnInit, Output} from '@angular/core';
 import {User} from "../../user";
 
 @Component({
   selector: 'proleit-user-list-item',
   templateUrl: './user-list-item.component.html',
-  styleUrls: ['./user-list-item.component.scss']
+  styleUrls: ['./user-list-item.component.scss'],
+
 })
 export class UserListItemComponent {
 
   @Input()
   user?: User;
+
+  @HostBinding('class.is-selected')
+  @Input()
+  isSelected = false;
 
   @Output()
   selectUsr: EventEmitter<User> = new EventEmitter<User>()
