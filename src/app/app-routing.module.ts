af7f679d080849aa869f49754471d846d70f68bb
Saban Ünlü [netTrek] <us@netTrek.de>
2022-11-11 11:47:46 +0100

    config routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {UserComponent} from "./user/user.component";
+import {CountdownComponent} from "./utils/countdown/countdown.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'user'},
+  { path: 'user', component: UserComponent },
+  { path: 'countdown', component: CountdownComponent },
+  { path: '**', redirectTo: 'user' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
