9a372c8fe771275a0cfa024e7f8ae95e2c4b7cb6
Saban Ünlü [netTrek] <us@netTrek.de>
2022-11-11 09:22:55 +0100

    getUsers final

--- src/app/user/user.service.ts
+++ src/app/user/user.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import {User} from "./user";
-import {BehaviorSubject, tap} from "rxjs";
+import {BehaviorSubject, map, tap} from "rxjs";
 import {HttpClient} from "@angular/common/http";
 import {environment} from "../../environments/environment";
 
@@ -12,33 +12,33 @@ export class UserService {
 
   selectedUsr$: BehaviorSubject<User | undefined> = new BehaviorSubject<User | undefined>( undefined );
   constructor( private readonly http: HttpClient ) {
-    console.log( http )
     this.init();
   }
-
   setSelectedUser(usr: User) {
     const crrUsr = this.selectedUsr$.value;
     this.selectedUsr$.next( usr === crrUsr ? undefined : usr );
   }
-
   delLastItem (){
     this.userList.pop();
   }
-
+  getUsers () {
+    return this.http.get<User[]>( environment.api )
+      .pipe(
+        map ( userList => userList.map( user => ({...user, birthday: new Date (user.birthday as any)})) ),
+        tap ( userlist => this.userList = userlist ),
+      );
+  }
   private init() {
     this.update();
   }
 
   private update() {
     console.log('update')
-    this.http.get<User[]>( environment.api )
-      .pipe(
-        tap ( console.log )
-      )
-      .subscribe(
-        {
-          next: value => this.userList = value
-        }
-      );
+    this.getUsers().subscribe();
+    // this.http.get<User[]>( environment.api )
+    //   .pipe(        tap ( console.log )      )
+    //   .subscribe(        {
+    //       next: value => this.userList = value
+    //     }      );
   }
 }
